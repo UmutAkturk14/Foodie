e506187a85105e0da069ffd5fe77512d1644cf15
Umut Akturk <umut.akturk14@gmail.com>
2025-07-19 21:43:06 +0200

    Deployment optimisation.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/FoodieGoodie/",
+  base: "/Foodie/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
